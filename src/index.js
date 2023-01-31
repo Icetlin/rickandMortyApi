28eca0b13eaecd456c5cf2c15ba9b88582e555c1
Ilia L <ilyaled@gmail.com>
2023-01-31 18:07:45 +0400

    1.1

--- src/index.js
+++ src/index.js
@@ -1,31 +1,18 @@
 import fetchCharsFromApiUrl from './api/fetchCharsFromApi.js'
 import createPagingButtons from './js/createPagingButtons.js'
-import createCharsTable from './js/createCharsTable.js'
-import {onClickOrigin} from './js/charsOriginOnClick.js'
 import {renderClicksOnChars} from './js/renderClicksOnChars.js'
 import {createBackButton} from './js/createBackButton.js'
 import {charsPageRender} from './js/charsPageRender.js'
 (async () => {
-    //x 
+     
     let chars = {};
-    
-    // const render = async(page) => {
-    //     chars = await fetchCharsFromApiUrl(page);
 
-    //     const table = createCharsTable(chars, onClickOrigin);
-
-    //     document.body.querySelector('table')?.remove();
-    //     document.body.append(table)
-        
-    //     history.replaceState(null, '', `/?page=${document.body.querySelector('table').dataset.originApiPage}`)
-    // }
-    chars = await fetchCharsFromApiUrl(page);
-    charsPageRender(chars);
-
-    const [nextPageButton, prevPageButton] = createPagingButtons(() => {
-        render(chars.info.next);
-    }, () => {
-        render(chars.info.prev);
+    const [nextPageButton, prevPageButton] = createPagingButtons(async() => {
+        chars = await fetchCharsFromApiUrl(chars.info.next)
+        charsPageRender(chars);
+    }, async() => {
+        chars = await fetchCharsFromApiUrl(chars.info.prev);
+        charsPageRender(chars);
     })
 
     document.body.appendChild(prevPageButton);
@@ -33,14 +20,16 @@ import {charsPageRender} from './js/charsPageRender.js'
     
          
     if (window.location.toString() === 'http://localhost:7777/page1' && !window.location.toString().includes('id') || window.location.toString() === 'http://localhost:7777/'){ // render main page
-        charsPageRender('https://rickandmortyapi.com/api/character'); 
+        chars = await fetchCharsFromApiUrl('https://rickandmortyapi.com/api/character');
+        charsPageRender(chars); 
         
 
     } // main page render
 
 
     if (window.location.toString().includes('page') && !window.location.toString().includes('id') && !window.location.toString().includes('page1')){ 
-        render(`https://rickandmortyapi.com/api/character${window.location.search}`);
+        chars = await fetchCharsFromApiUrl(`https://rickandmortyapi.com/api/character${window.location.search}`);
+        charsPageRender(chars); 
             
 
     } // others pages render
