56554ebf1d9fcdcf22a45159c71d0f0da1a8f7c1
Ilia L <ilyaled@gmail.com>
2023-02-03 17:19:39 +0400

    feature

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import {charsPageRender} from './js/charsPageRender.js'
 (async () => {
      
     let chars = {};
-    const pageUrl = window.location;
-    const pageLocation = pageUrl.toString();
-    const pageSearch = pageUrl.search;
+    let pageUrl = window.location;
+    let pageLocation = pageUrl.toString();
+    let pageSearch = pageUrl.search;
 
     const [nextPageButton, prevPageButton] = createPagingButtons(async() => {
         chars = await fetchCharsFromApiUrl(chars.info.next);
@@ -22,14 +22,16 @@ import {charsPageRender} from './js/charsPageRender.js'
 
     const goToCharList = async () => {
         chars = await fetchCharsFromApiUrl(`https://rickandmortyapi.com/api/character${pageSearch}`);
-        charsPageRender(chars);
+        document.body.appendChild(prevPageButton); 
+        document.body.appendChild(nextPageButton);
+        charsPageRender(chars); 
     }
 
     const goToCharInfo = () => {
-        const currentUrlSearchParams = new URLSearchParams(pageSearch);
-        const currentCharPage = currentUrlSearchParams.get('page').toString();
-        const currentCharId = currentUrlSearchParams.get('id').toString(); 
-        const backButton = createBackButton(() => {
+        let currentUrlSearchParams = new URLSearchParams(pageSearch);
+        let currentCharPage = currentUrlSearchParams.get('page').toString();
+        let currentCharId = currentUrlSearchParams.get('id').toString(); 
+        let backButton = createBackButton(() => {
         window.history.back()
         }, 
         'back to list');
@@ -38,8 +40,7 @@ import {charsPageRender} from './js/charsPageRender.js'
     }
 
 
-    document.body.appendChild(prevPageButton); 
-    document.body.appendChild(nextPageButton); 
+   
 
 
     window.addEventListener('popstate', async function (){
@@ -47,8 +48,6 @@ import {charsPageRender} from './js/charsPageRender.js'
        if(!currentUrl.includes('id')){
         document.getElementById('backButton')?.remove();
         document.body.querySelector('table')?.remove();
-        document.body.appendChild(prevPageButton); 
-        document.body.appendChild(nextPageButton); 
         goToCharList();
        }
     })
